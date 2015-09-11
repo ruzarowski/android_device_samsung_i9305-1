b6e5cd934ae8dc33b3ef0900c4c3c770f26f45d1
Rafael Uzarowski <rafael.uzarowski@sevenval.com>
2015-09-11 13:21:43 +0200

    libUMP_intermediates

--- Android.mk
+++ Android.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libUMP_intermediates)
+$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libUMP_intermediates/export_includes)
+
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),i9305)
